ae6c062ea6af365cbde3f9d1ce27c2954277722f
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-09-07 20:53:00 +0000

    chore: agregar modelo de fiestero en modulo fiestero

--- fiestero/serializer.py
+++ fiestero/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Favorito
+from .models import Favorito, Fiestero
 
 class FavoritotoSerializer(serializers.ModelSerializer):
     class Meta:
         model =  Favorito
-        fields = '__all__'
+        fields = '_all_'
+
+class FiesteroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Fiestero
+        fields = '_all_'
