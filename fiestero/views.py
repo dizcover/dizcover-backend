b0a7bd07e4274b03a4f6d025f4cf091da616cade
SrRobls <joroblesr@unal.edu.co>
2024-09-07 16:30:21 -0500

    relacion fiestero-favoritos-establecimiento

--- fiestero/views.py
+++ fiestero/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import FavoritotoSerializer
-from .models import Favorito
+from .models import Favorito, Fiestero
+from rest_framework.response import Response
+
 # Create your views here.
 
 
 class FavoritoViewSet(viewsets.ModelViewSet):
     queryset = Favorito.objects.all()
     serializer_class = FavoritotoSerializer
+
+    def create(self, request, *args, **kwargs):
+        print(request)
+        usuario = request.user.fiestero
+        establecimiento = request.data.get('establecimiento')
+
+        if Favorito.objects.filter(fiestero=usuario, establecimiento=establecimiento).exists():
+            return Response({'error': 'Ya existe el favorito'}, status=400)
+        
+        return super().create(request, *args, **kwargs)
