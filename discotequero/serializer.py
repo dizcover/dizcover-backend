5dbeefd7324172a676dddf90af60e5c077fcf77a
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-09-15 13:11:40 +0000

    fix: arreglar typo y mejorar documentacion en modulo discotequero

--- discotequero/serializer.py
+++ discotequero/serializer.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Discotequero
 
 class DiscotequeroSerializer(serializers.ModelSerializer):
-
+    """
+    Este serializador convierte el modelo 'Discotequero' a formatos fácilmente intercambiables (JSON).
+    Utiliza el Meta para especificar que se deben incluir todos los campos del modelo.
+    """
 
     class Meta:
         model = Discotequero
--- discotequero/views.py
+++ discotequero/views.py
@@ -1,8 +1,8 @@
 
 from rest_framework import viewsets
-from .serializer import DicotequeroSerializer
+from .serializer import DiscotequeroSerializer
 from .models import Discotequero
 
 class DicotequeroViewSet(viewsets.ModelViewSet):
     queryset = Discotequero.objects.all()
-    serializer_class = DicotequeroSerializer
+    serializer_class = DiscotequeroSerializer
